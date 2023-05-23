676f2c1a4672071c7bd892931a27969a6794c0fd
Imran Fazilov <ifazilov3584@bths.edu>
2023-05-23 12:29:51 -0400

    probably switch idea

--- src/ExchangeAPI.java
+++ src/ExchangeAPI.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class ExchangeAPI {
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+
+public class ExchangeAPI {
+    public static void getExchangeRate(String from, String to) {
+        String APIkey = "3EDDBB5A-0489-42C2-B575-8693D20CB312";
+        String queryParameters = from + "/" + to;
+        String endpoint = "https://rest.coinapi.io/v1/exchangerate/";
+        String url = endpoint + queryParameters;
+        String urlResponse = "";
+        try {
+            URI myUri = URI.create(url); // creates a URI object from the url string
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(myUri)
+                    .header("X-CoinAPI-Key", APIkey)
+                    .build();
+            HttpClient client = HttpClient.newHttpClient();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            urlResponse = response.body();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println(urlResponse);
+    }
 }
